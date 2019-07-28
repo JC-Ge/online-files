cb2f184ca99db27552f2e13b7f027d8e7624bae4
Ge Xianglin <gxl200896@163.com>
2019-07-29 00:28:58 +0800

    Update fresh2.js

--- fresh2.js
+++ fresh2.js
@@ -1,12 +1,12 @@
-"{"data": [{
-	orgCode: "1234",
-	topbg: baseURL + 'indexTop.png',
-	orgLogo: "http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxorg.jpg",
-	orgName: "蟹状元大闸蟹股份有限公司",
-	module: [{
-			icon: "http://chain.ufile-inspur.ucloud.com.cn/fresh/qyzz.png",
-			titlename: "企业资质",
-			item: [
+"{"data"": [{
+	"orgCode": "1234",
+	"topbg": "http://chain.ufile-inspur.ucloud.com.cn/fresh/indexTop.png',
+	"orgLogo": "http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxorg.jpg",
+	"orgName": "蟹状元大闸蟹股份有限公司",
+	"module": [{
+			"icon": "http://chain.ufile-inspur.ucloud.com.cn/fresh/qyzz.png",
+			"titlename": "企业资质",
+			"item": [
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxqyzz1.png",
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxqyzz2.png",
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxqyzz3.png",
@@ -15,9 +15,9 @@
 			]
 		},
 		{
-			icon: "http://chain.ufile-inspur.ucloud.com.cn/fresh/yzhgs.png",
-			titlename: "养殖户故事",
-			item: [
+			"icon": "http://chain.ufile-inspur.ucloud.com.cn/fresh/yzhgs.png",
+			"titlename": "养殖户故事",
+			"item": [
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxgs1.png",
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxgs2.png",
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxgs3.png",
@@ -35,17 +35,17 @@
 			]
 		},
 		{
-			icon: "http://chain.ufile-inspur.ucloud.com.cn/fresh/szhj.png",
-			titlename: "生长环境",
-			item: [
+			"icon": "http://chain.ufile-inspur.ucloud.com.cn/fresh/szhj.png",
+			"titlename": "生长环境",
+			"item": [
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxszhj1.png",
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxszhj1.png",
 			]
 		},
 		{
-			icon: "http://chain.ufile-inspur.ucloud.com.cn/fresh/yzgc.png",
-			titlename: "养殖过程",
-			item: [
+			"icon": "http://chain.ufile-inspur.ucloud.com.cn/fresh/yzgc.png",
+			"titlename": "养殖过程",
+			"item": [
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxyz1.png",
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxyz2.png",
 				"http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxyz3.png",
@@ -53,22 +53,22 @@
 			]
 		},
 		{
-			icon: "http://chain.ufile-inspur.ucloud.com.cn/fresh/hzqy.png",
-			titlename: "合作企业",
-			item: [{
-					name: "苏州泉屋百货有限公司",
-					logo: "http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxszqw.png",
-					desc: "苏州泉屋百货有限公司于2008年05月21日在苏州高新区（虎丘区）市场监督管理局登记成立。法定代表人四条晴也，公司经营范围包括批发、零售：预包装食品兼散装食品，乳制品（含婴幼儿配方乳粉）等。"
+			"icon": "http://chain.ufile-inspur.ucloud.com.cn/fresh/hzqy.png",
+			"titlename": "合作企业",
+			"item": [{
+					"name": "苏州泉屋百货有限公司",
+					"logo": "http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxszqw.png",
+					"desc": "苏州泉屋百货有限公司于2008年05月21日在苏州高新区（虎丘区）市场监督管理局登记成立。法定代表人四条晴也，公司经营范围包括批发、零售：预包装食品兼散装食品，乳制品（含婴幼儿配方乳粉）等。"
 				},
 				{
-					name: "河南新康桥物业服务有限公司",
-					logo: "http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxhnkq.png",
-					desc: "河南新康桥物业服务有限公司于2007年8月31日在郑州市工商行政管理局二七分局登记成立。法定代表人周巧红，公司经营范围包括物业经营与管理（凭资质证经营）；酒店管理等。"
+					"name": "河南新康桥物业服务有限公司",
+					"logo": "http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxhnkq.png",
+					"desc": "河南新康桥物业服务有限公司于2007年8月31日在郑州市工商行政管理局二七分局登记成立。法定代表人周巧红，公司经营范围包括物业经营与管理（凭资质证经营）；酒店管理等。"
 				},
 				{
-					name: "浙江绿城物业园区生活服务有限公司",
-					logo: "http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxzjlc.png",
-					desc: "浙江绿城物业园区生活服务有限公司于2009年04月22日在浙江省工商行政管理局登记成立。法定代表人陈浩，公司经营范围包括预包装食品、乳制品（不含婴幼儿配方乳粉）等。"
+					"name": "浙江绿城物业园区生活服务有限公司",
+					"logo": "http://chain.ufile-inspur.ucloud.com.cn/fresh/dzxzjlc.png",
+					"desc": "浙江绿城物业园区生活服务有限公司于2009年04月22日在浙江省工商行政管理局登记成立。法定代表人陈浩，公司经营范围包括预包装食品、乳制品（不含婴幼儿配方乳粉）等。"
 				},
 			]
 		},
